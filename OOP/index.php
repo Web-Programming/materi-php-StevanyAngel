590a08789a01ceacca8dd888e4e2ee9a5479e185
StevanyAngel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-04-12 18:23:15 +0700

    Metode

--- OOP/index.php
+++ OOP/index.php
@@ -7,9 +7,19 @@ class robot{
     public $suara = 'ngik-ngik';
     public $berat = 30;
 
+    //metode
+    public function bersuara(){
+        echo 'suara robotnya ... ' . $this->suara . ' ';
+    }
+
+    public function berat_robot(){
+        return $this->berat;
+    }
+
 }
 
 $robot1 = new robot;
-echo 'suara robotnya... ' . $robot1->suara . ' beratnya ' . $robot1->berat;
+$robot1->bersuara();
+echo $robot1->berat_robot();
 
 ?>
