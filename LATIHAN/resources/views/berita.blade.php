05e4026cf61c3f533154676c00c567399566b3fb
StevanyAngel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-04-29 20:32:47 +0700

    Env
    Make view

---
+++ LATIHAN/resources/views/berita.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Berita</h1>
+    <b2>ID: {{ $id }}</b2>
+    <b2>Judul: {{ $judul }}</b2>
+</div>
--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beranda', function () {
+    return view('beranda', 
+    [
+        'name' => 'Angel',
+        'email' => 'angelstvny10@gmail.com',
+        'alamat' => 'Palembang']);
+});
+
+Route::get('/berita/', function ($id, $judul = null) {
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+});
